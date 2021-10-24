1ee330994a3f434be023463d9a1e3e6426989806
BlackViper3 <yagzzan@gmail.com>
2021-10-24 16:37:27 +0530

    Adding ColoredPrinter.java and Property files

---
+++ src/main/java/coloredprinter/ColoredPrinter.java
@@ -0,0 +1,38 @@
+package coloredprinter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+public class ColoredPrinter {
+
+    public static HashMap<String, String> colorAndUnicodeMap = new HashMap<>();
+
+
+    static {
+        Properties colorCodeProperties = new Properties();
+        try {
+            colorCodeProperties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("ANSII_Color_Code.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        colorAndUnicodeMap = (HashMap<String, String>) colorCodeProperties
+                .entrySet()
+                .stream()
+                .collect(
+                        Collectors.toMap(
+                                entry -> entry.getKey().toString(),
+                                entry -> entry.getValue().toString())
+                );
+    }
+
+    public void coloredSysOut(String Text, String Color) {
+        if (colorAndUnicodeMap.get(Color) != null)
+            System.out.println(colorAndUnicodeMap.get(Color) + Text + colorAndUnicodeMap.get("RESET"));
+        else
+            System.out.println(Text);;
+    }
+
+
+}
